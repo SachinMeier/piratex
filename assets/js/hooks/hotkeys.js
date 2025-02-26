9da869f60193c0e2e992e25c2a959bd4d7c26d1e
Sachin Meier <sachin.meier@river.com>
2025-02-26 00:25:43 -0500

    allow hotkeys even from within the text entry

--- assets/js/hooks/hotkeys.js
+++ assets/js/hooks/hotkeys.js
@@ -1,10 +1,11 @@
 export const Hotkeys = {
-	 // NOTE: this setup hits the server every time a key in the hotkeys set is pressed.
-    // This is not ideal, but it's a good start.
+	// NOTE: this setup hits the server every time a key in the hotkeys set is pressed.
+	// This is not ideal, but it's a good start.
 	mounted() {
 	  window.addEventListener("keydown", (event) => {
-		// ignore event from forms/text inputs
-		if (["INPUT", "SELECT", "TEXTAREA"].includes(event.target.tagName)) return;
+		// ignore event from forms/text inputs. We actually DONT do this
+		// and instead only use non-letters as hotkeys.
+		// if (["INPUT", "SELECT", "TEXTAREA"].includes(event.target.tagName)) return;
 
 		// minimize hotkey handling to only the ones we care about
 		if (![" "].includes(event.key)) return;
