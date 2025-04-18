b73d07d8efa70a75a51d3ef1d8f8dcff201b7ab8
Sachin Meier <sachin.meier@river.com>
2025-04-18 19:38:07 -0400

    filter words by length before calculating products

--- lib/piratex/explorer.ex
+++ lib/piratex/explorer.ex
@@ -4,24 +4,36 @@ defmodule Piratex.Services.Explorer do
 
   def froms(word) do
     word_product = WordClaimService.calculate_word_product(word)
+    word_length = String.length(word)
 
     Dictionary.words()
     |> Enum.filter(fn word ->
-      word
-      |> WordClaimService.calculate_word_product()
-      |> then(&rem(word_product, &1) == 0 and &1 != word_product)
+      # first filter out longer words
+      if String.length(word) > word_length do
+        word
+        |> WordClaimService.calculate_word_product()
+        |> then(&rem(word_product, &1) == 0 and &1 != word_product)
+      else
+        false
+      end
     end)
     |> Enum.sort_by(&String.length/1, :desc)
   end
 
   def tos(word) do
     word_product = WordClaimService.calculate_word_product(word)
+    word_length = String.length(word)
 
     Dictionary.words()
     |> Enum.filter(fn word ->
-      word
-      |> WordClaimService.calculate_word_product()
-      |> then(&rem(&1, word_product) == 0 and &1 != word_product)
+      # first filter out shorter words
+      if String.length(word) < word_length do
+        word
+        |> WordClaimService.calculate_word_product()
+        |> then(&rem(&1, word_product) == 0 and &1 != word_product)
+      else
+        false
+      end
     end)
     |> Enum.sort_by(&String.length/1, :asc)
   end
